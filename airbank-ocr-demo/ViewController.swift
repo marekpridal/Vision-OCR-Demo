2bcfafc79bee0a0abe832630c954cecb5f069358
Marek Pridal <marek@marekpridal.eu>
2019-11-18 11:32:32 +0100

    Initial Commit

---
+++ airbank-ocr-demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  airbank-ocr-demo
+//
+//  Created by Marek Přidal on 18/11/2019.
+//  Copyright © 2019 Marek Přidal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
