ffcf9325e1295487e933cd0a19ff56ca68c8c35c
Marek Pridal <marek@marekpridal.eu>
2019-11-18 13:11:54 +0100

    First implementation of native OCR

---
+++ airbank-ocr-demo/PreviewView.swift
@@ -0,0 +1,105 @@
+//
+//  PreviewView.swift
+//  VisionDetection
+//
+//  Created by Wei Chieh Tseng on 09/06/2017.
+//  Copyright © 2017 Willjay. All rights reserved.
+//
+
+import UIKit
+import Vision
+import AVFoundation
+
+class PreviewView: UIView {
+    
+    private var maskLayer = [CAShapeLayer]()
+    
+    // MARK: AV capture properties
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer {
+        return layer as! AVCaptureVideoPreviewLayer
+    }
+    
+    var session: AVCaptureSession? {
+        get {
+            return videoPreviewLayer.session
+        }
+        
+        set {
+            videoPreviewLayer.session = newValue
+        }
+    }
+    
+    override class var layerClass: AnyClass {
+        return AVCaptureVideoPreviewLayer.self
+    }
+    
+    // Create a new layer drawing the bounding box
+    private func createLayer(in rect: CGRect) -> CAShapeLayer{
+        
+        let mask = CAShapeLayer()
+        mask.frame = rect
+        mask.cornerRadius = 10
+        mask.opacity = 0.75
+        mask.borderColor = UIColor.yellow.cgColor
+        mask.borderWidth = 2.0
+        
+        maskLayer.append(mask)
+        layer.insertSublayer(mask, at: 1)
+        
+        return mask
+    }
+    
+    func drawFaceboundingBox(face : VNFaceObservation) {
+        
+        let transform = CGAffineTransform(scaleX: 1, y: -1).translatedBy(x: 0, y: -frame.height)
+        
+        let translate = CGAffineTransform.identity.scaledBy(x: frame.width, y: frame.height)
+        
+        // The coordinates are normalized to the dimensions of the processed image, with the origin at the image's lower-left corner.
+        let facebounds = face.boundingBox.applying(translate).applying(transform)
+        
+        _ = createLayer(in: facebounds)
+        
+    }
+    
+    private func drawPointsOnLayer(rect:CGRect, landmarkPoints: [CGPoint], isClosed: Bool = true) -> CALayer {
+        let linePath = UIBezierPath()
+        linePath.move(to: landmarkPoints.first!)
+        
+        for point in landmarkPoints.dropFirst() {
+            linePath.addLine(to: point)
+        }
+        
+        if isClosed {
+            linePath.addLine(to: landmarkPoints.first!)
+        }
+        
+        let lineLayer = CAShapeLayer()
+        lineLayer.path = linePath.cgPath
+        lineLayer.fillColor = nil
+        lineLayer.opacity = 1.0
+        lineLayer.strokeColor = UIColor.green.cgColor
+        lineLayer.lineWidth = 0.02
+        
+        return lineLayer
+    }
+    
+    func drawRect(textObservation: VNRecognizedTextObservation) {
+        let transform = CGAffineTransform(scaleX: 1, y: -1).translatedBy(x: 0, y: -frame.height)
+        
+        let translate = CGAffineTransform.identity.scaledBy(x: frame.width, y: frame.height)
+        
+        // The coordinates are normalized to the dimensions of the processed image, with the origin at the image's lower-left corner.
+        let textBounds = textObservation.boundingBox.applying(translate).applying(transform)
+        
+        _ = createLayer(in: textBounds)
+    }
+    
+    func removeMask() {
+        for mask in maskLayer {
+            mask.removeFromSuperlayer()
+        }
+        maskLayer.removeAll()
+    }
+    
+}
--- airbank-ocr-demo/ViewController.swift
+++ airbank-ocr-demo/ViewController.swift
@@ -6,15 +6,155 @@
 //  Copyright © 2019 Marek Přidal. All rights reserved.
 //
 
+import Photos
 import UIKit
+import Vision
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+    lazy var textDetectionRequest: VNRecognizeTextRequest = {
+        let request = VNRecognizeTextRequest(completionHandler: self.handleDetectedText)
+        request.recognitionLevel = .accurate
+        request.recognitionLanguages = ["cs_CZ", "en_GB"]
+        return request
+    }()
+    
+    private lazy var uploadPhotoButton: UIButton = {
+        let button = UIButton(type: UIButton.ButtonType.roundedRect)
+        button.addTarget(self, action: #selector(presentImageActionSheet), for: .touchUpInside)
+        button.setTitle("Upload picture", for: .normal)
+        return button
+    }()
+    
+    private lazy var imageView: UIImageView = {
+       let imageView = UIImageView()
+        return imageView
+    }()
+    
+    private lazy var previewView: PreviewView = {
+       PreviewView()
+    }()
+    
+    private let pickerController: UIImagePickerController = .init()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        pickerController.delegate = self
+        setupLayout()
+    }
+    
+    private func process(image: UIImage) {
+        guard let cgImage = image.cgImage else { return }
+        
+        let requests = [textDetectionRequest]
+        let imageRequestHandler = VNImageRequestHandler(cgImage: cgImage, orientation: .right, options: [:])
+        DispatchQueue.global(qos: .userInitiated).async {
+            do {
+                try imageRequestHandler.perform(requests)
+            } catch let error {
+                print("Error: \(error)")
+            }
+        }
+    }
+    
+    private func handleDetectedText(request: VNRequest?, error: Error?) {
+        if let error = error {
+            print(error.localizedDescription)
+            return
+        }
+        guard let results = request?.results, results.count > 0 else {
+            print("No text was found.")
+            return
+        }
+        
+        drawVisionRequestResults((results as? [VNRecognizedTextObservation]) ?? [])
+        
+        for result in results {
+            if let observation = result as? VNRecognizedTextObservation {
+                print(observation)
+            }
+        }
+    }
+    
+    private func setupLayout() {
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(imageView)
+        imageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        imageView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        imageView.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor).isActive = true
+        imageView.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor).isActive = true
+        imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor, multiplier: 3/2).isActive = true
+        
+        previewView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(previewView)
+        previewView.leadingAnchor.constraint(equalTo: imageView.leadingAnchor).isActive = true
+        previewView.trailingAnchor.constraint(equalTo: imageView.trailingAnchor).isActive = true
+        previewView.bottomAnchor.constraint(equalTo: imageView.bottomAnchor).isActive = true
+        previewView.topAnchor.constraint(equalTo: imageView.topAnchor).isActive = true
+        
+        uploadPhotoButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(uploadPhotoButton)
+        uploadPhotoButton.leadingAnchor.constraint(equalToSystemSpacingAfter: view.safeAreaLayoutGuide.leadingAnchor, multiplier: 0).isActive = true
+        uploadPhotoButton.trailingAnchor.constraint(equalToSystemSpacingAfter: view.safeAreaLayoutGuide.trailingAnchor, multiplier: 0).isActive = true
+        uploadPhotoButton.bottomAnchor.constraint(equalToSystemSpacingBelow: view.safeAreaLayoutGuide.bottomAnchor, multiplier: 0).isActive = true
+        uploadPhotoButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
     }
 
+    private func action(for type: UIImagePickerController.SourceType, title: String) -> UIAlertAction? {
+        guard UIImagePickerController.isSourceTypeAvailable(type) else { return nil }
 
+        return UIAlertAction(title: title, style: .default) { [weak self] _ in
+            guard let self = self else { return }
+            self.pickerController.sourceType = type
+            self.present(self.pickerController, animated: true)
+        }
+    }
+
+    @objc
+    private func presentImageActionSheet() {
+        let alertController: UIAlertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+
+        if let action: UIAlertAction = action(for: .camera, title: "Take photo") {
+            alertController.addAction(action)
+        }
+        if let action: UIAlertAction = action(for: .savedPhotosAlbum, title: "Saved photos album") {
+            alertController.addAction(action)
+        }
+        if let action: UIAlertAction = action(for: .photoLibrary, title: "Photo library") {
+            alertController.addAction(action)
+        }
+
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+
+        if UIDevice.current.userInterfaceIdiom == .pad {
+            alertController.popoverPresentationController?.sourceView = view
+            alertController.popoverPresentationController?.sourceRect = view.bounds
+            alertController.popoverPresentationController?.permittedArrowDirections = [.down, .up]
+        }
+
+        present(alertController, animated: true)
+    }
+    
+    func drawVisionRequestResults(_ results: [VNRecognizedTextObservation]) {
+        DispatchQueue.main.async { [weak self] in
+            self?.previewView.removeMask()
+            for textObservation in results {
+                self?.previewView.drawRect(textObservation: textObservation)
+            }
+        }
+    }
 }
 
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        dismiss(animated: true, completion: nil)
+        guard let image = info[.originalImage] as? UIImage else { return }
+        imageView.image = image
+        process(image: image)
+    }
+    
+}
